3221c619f8e0a3a740be86f69fee9d49ff8480f5
aidendenden233 <2996876880@qq.com>
2023-07-27 20:02:44 +0800

    update

--- Assets/工程文件夹/AAAScenes_THERE/Scripts/PlayerManager.cs
+++ Assets/工程文件夹/AAAScenes_THERE/Scripts/PlayerManager.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     //��������
-    public ChangeItem _changeItem;
-    public PhotoMode _photoMode;
+    //public ChangeItem _changeItem;
+    //public PhotoMode _photoMode;
     
 
     private void Update()
     {
-        checkCanChangeItem();
+        //checkCanChangeItem();
     }
 
-   void checkCanChangeItem()
-    {
-        if (_photoMode.galleryPanel.activeSelf || _photoMode.photoModePanel.activeSelf || _photoMode.photoSavePanel.activeSelf)
-        {
-            _changeItem.IsCanChangeItem = false;
-        }
-        else
-        {
-            _changeItem.IsCanChangeItem = true;
-        }
-    }
-
-
-
+   // void checkCanChangeItem()
+   //  {
+   //      if (_photoMode.galleryPanel.activeSelf || _photoMode.photoModePanel.activeSelf || _photoMode.photoSavePanel.activeSelf)
+   //      {
+   //          _changeItem.IsCanChangeItem = false;
+   //      }
+   //      else
+   //      {
+   //          _changeItem.IsCanChangeItem = true;
+   //      }
+   //  }
 
 }
