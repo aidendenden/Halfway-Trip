b09ac3ae80ffc45368e02e115e0d563f9ae6c106
kao_jun <863646260@qq.com>
2023-08-20 22:29:16 +0800

    小小镜像

--- Assets/工程文件夹/GameJamCode/SyncRotationAndScale.cs
+++ Assets/工程文件夹/GameJamCode/SyncRotationAndScale.cs
@@ -29,7 +29,15 @@ public class SyncRotationAndScale : MonoBehaviour
         float scaledRatio = Mathf.Pow(scaleRatio, distance / maxDistance);
         transform.localScale = initialScale * scale * scaledRatio;
 
-       
+
+
+        Vector3 targetPosition = target.position;
+
+        // 计算在yz平面上的镜像对称位置
+        Vector3 mirroredPosition = new Vector3(gameObject.transform.position.x, targetPosition.y, targetPosition.z);
+
+        // 设置物体的位置为镜像对称位置
+        transform.position = mirroredPosition;
     }
 
 
